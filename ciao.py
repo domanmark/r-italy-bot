76183b0de2a79caaee9c38e07387a1702dda3d13
domanmark <mpd.github@gmail.com>
2020-05-11 21:18:31 -0400

    get comments and add NLP functions

--- ciao.py
+++ ciao.py
@@ -1,6 +1,27 @@
 import praw
+import spacy
 
-reddit = praw.Reddit("r-italy-bot")
 
-for submission in reddit.subreddit("italy").hot(limit=10):
-    print(submission.title)
+def main():
+    reddit = praw.Reddit("r-italy-bot")
+
+    all_comments = []
+
+    for submission in reddit.subreddit("italy").hot(limit=10):
+        submission.comments.replace_more(limit=None)
+        for comment in submission.comments.list():
+            all_comments.append(comment.body)
+
+
+def is_valid_token(token):
+    if not token or not token.string.strip() or token.is_stop or token.is_punct:
+        return False
+    return True
+
+
+def preprocess_token(token):
+    return token.lemma_.strip().lower()
+
+
+if __name__ == "__main__":
+    main()
